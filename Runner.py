85007073d69610534edc9494117ec02db099b8ea
Martin Ayasse <martinay@outlook.com>
2017-12-28 16:23:27 +0100

    add multithreading

---
+++ Runner.py
@@ -0,0 +1,77 @@
+from TaskQueue import TaskQueue
+from connect4.c4Scenario1 import c4Scenario1
+from connect4.c4Scenario10 import c4Scenario10
+from connect4.c4Scenario10a import c4Scenario10a
+from connect4.c4Scenario10aa import c4Scenario10aa
+from connect4.c4Scenario10aaa import c4Scenario10aaa
+from connect4.c4Scenario10aaaa import c4Scenario10aaaa
+from connect4.c4Scenario10b import c4Scenario10b
+from connect4.c4Scenario10bb import c4Scenario10bb
+from connect4.c4Scenario10c import c4Scenario10c
+from connect4.c4Scenario10cc import c4Scenario10cc
+from connect4.c4Scenario10ccc import c4Scenario10ccc
+from connect4.c4Scenario1a import c4Scenario1a
+from connect4.c4Scenario2 import c4Scenario2
+from connect4.c4Scenario3 import c4Scenario3
+from connect4.c4Scenario4 import c4Scenario4
+from connect4.c4Scenario5 import c4Scenario5
+from connect4.c4Scenario6 import c4Scenario6
+from connect4.c4Scenario7 import c4Scenario7
+from connect4.c4Scenario8 import c4Scenario8
+from connect4.c4Scenario9 import c4Scenario9
+from scenario_runner import ScenarioRunner
+
+max_epoches=30
+nr_of_executions = 20
+use_Gpu = False
+num_workers_threads = 4
+
+if use_Gpu:
+    import tensorflow as tf
+    from keras.backend.tensorflow_backend import set_session
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.1)
+    config = tf.ConfigProto(gpu_options=gpu_options)
+    config.gpu_options.allow_growth = True
+    config.gpu_options.visible_device_list = "0"
+    #session = tf.Session(config=config)
+    set_session(tf.Session(config=config))
+
+scenarios = [#c4Scenario1(),
+             #c4Scenario1a(),
+             #c4Scenario2(),
+             #c4Scenario3(),
+             #c4Scenario4(),
+             #c4Scenario5(),
+             #c4Scenario6(),
+             #c4Scenario7(),
+             #c4Scenario8(),
+             #c4Scenario9(),
+             #c4Scenario10(),
+             #c4Scenario10a(),
+             #c4Scenario10aa(),
+             #c4Scenario10aaa(),
+             #c4Scenario10aaaa(),
+             #c4Scenario10b(),
+             #c4Scenario10bb(),
+             #c4Scenario10c(),
+             c4Scenario10cc(),
+             c4Scenario10ccc()
+        #########################breakthrough#########################
+             ]
+
+
+def runScenario(scenario, max_epoches, nr_of_executions):
+    print('Scenario {} started execution'.format(scenario.__class__.__name__))
+    runner = ScenarioRunner()
+    runner.init(scenario)
+    runner.run(max_epoches=max_epoches, nr_of_executions=nr_of_executions)
+
+
+q = TaskQueue(num_workers=num_workers_threads)
+
+for scenario in scenarios:
+    q.add_task(runScenario, scenario, max_epoches=max_epoches, nr_of_executions=nr_of_executions)
+    print('Scenario {} loaded and added'.format(scenario.__class__.__name__))
+
+q.join()
+print('finished')
---
+++ TaskQueue.py
@@ -0,0 +1,28 @@
+from threading import Thread
+import queue
+
+#source by Shashwat Kumar https://medium.com/@shashwat_ds/a-tiny-multi-threaded-job-queue-in-30-lines-of-python-a344c3f3f7f0
+class TaskQueue(queue.Queue):
+
+    def __init__(self, num_workers=1):
+        queue.Queue.__init__(self)
+        self.num_workers = num_workers
+        self.start_workers()
+
+    def add_task(self, task, *args, **kwargs):
+        args = args or ()
+        kwargs = kwargs or {}
+        self.put((task, args, kwargs))
+
+    def start_workers(self):
+        for i in range(self.num_workers):
+            t = Thread(target=self.worker)
+            t.daemon = True
+            t.start()
+
+    def worker(self):
+        while True:
+            tupl = self.get()
+            item, args, kwargs = self.get()
+            item(*args, **kwargs)
+            self.task_done()
